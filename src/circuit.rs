66969d2a1b7dabc0a0b47b826ab8ffba540c8629
Remco Bloemen <remco@wicked.ventures>
2022-07-21 12:29:34 -0400

    Benchmark

--- src/circuit.rs
+++ src/circuit.rs
@@ -1,57 +1,152 @@
+use std::time::Instant;
+
 use eyre::{eyre, Result};
 use plonky2::{
-    field::{goldilocks_field::GoldilocksField, types::Field},
-    iop::{
-        target::Target,
-        witness::{PartialWitness, Witness},
+    field::{
+        goldilocks_field::GoldilocksField, secp256k1_base::Secp256K1Base,
+        secp256k1_scalar::Secp256K1Scalar, types::PrimeField,
     },
+    iop::witness::PartialWitness,
     plonk::{
         circuit_builder::CircuitBuilder,
         circuit_data::{CircuitConfig, CircuitData},
-        config::{GenericConfig, KeccakGoldilocksConfig, PoseidonGoldilocksConfig},
-        proof::{CompressedProofWithPublicInputs, ProofWithPublicInputs},
+        config::{GenericConfig, PoseidonGoldilocksConfig},
+        proof::CompressedProofWithPublicInputs,
     },
 };
-use tracing::{info, info_span, log, span, Level};
+use plonky2_ecdsa::{
+    curve::secp256k1::Secp256K1,
+    gadgets::{
+        biguint::{
+            witness_get_biguint_target, witness_set_biguint_target, BigUintTarget,
+            CircuitBuilderBiguint,
+        },
+        curve::AffinePointTarget,
+        ecdsa::{verify_message_circuit, ECDSAPublicKeyTarget, ECDSASignatureTarget},
+        nonnative::{CircuitBuilderNonNative, NonNativeTarget},
+    },
+};
+use tracing::{event, info, info_span, log, span, Level, warn};
+
+use crate::{MessageHash, PublicKey, Signature};
 
 const D: usize = 2;
 type C = PoseidonGoldilocksConfig;
 type F = <C as GenericConfig<D>>::F;
 type Builder = CircuitBuilder<F, D>;
 type Proof = CompressedProofWithPublicInputs<F, C, D>;
+type Inputs = PartialWitness<GoldilocksField>;
 
 pub struct Circuit {
-    inputs:  Vec<Target>,
-    outputs: Vec<Target>,
-    data:    CircuitData<F, C, D>,
+    inputs: Vec<SignatureInput>,
+    data:   CircuitData<F, C, D>,
+}
+
+struct FieldInput<F>
+where
+    F: PrimeField,
+{
+    pub biguint:    BigUintTarget,
+    pub non_native: NonNativeTarget<F>,
+}
+
+impl<F: PrimeField> FieldInput<F> {
+    pub fn new(builder: &mut Builder, public: bool) -> Self {
+        let biguint = builder.add_virtual_biguint_target(Builder::num_nonnative_limbs::<F>());
+        if public {
+            biguint.limbs.iter().for_each(|&limb| {
+                builder.register_public_input(limb.0);
+            });
+        }
+        let non_native = builder.biguint_to_nonnative(&biguint);
+        Self {
+            biguint,
+            non_native,
+        }
+    }
+
+    pub fn set(&self, witness: &mut Inputs, value: F) {
+        witness_set_biguint_target(witness, &self.biguint, &value.to_canonical_biguint());
+    }
+
+    pub fn get(&self, witness: &Inputs) -> F {
+        let n = witness_get_biguint_target(witness, &self.biguint);
+        F::from_biguint(n)
+    }
+}
+
+struct SignatureInput {
+    pub pubkey_x: FieldInput<Secp256K1Base>,
+    pub pubkey_y: FieldInput<Secp256K1Base>,
+    pub msg:      FieldInput<Secp256K1Scalar>,
+    pub sig_r:    FieldInput<Secp256K1Scalar>,
+    pub sig_s:    FieldInput<Secp256K1Scalar>,
+}
+
+impl SignatureInput {
+    pub fn new(builder: &mut Builder) -> Self {
+        let span = info_span!("SignatureInput::new");
+        let _guard = span.enter();
+        builder.push_context(log::Level::Info, "Secp256K1Verifier");
+
+        // Inputs
+        builder.push_context(log::Level::Info, "inputs");
+        let si = info_span!("inputs").in_scope(|| Self {
+            pubkey_x: FieldInput::new(builder, true),
+            pubkey_y: FieldInput::new(builder, true),
+            msg:      FieldInput::new(builder, true),
+            sig_r:    FieldInput::new(builder, false),
+            sig_s:    FieldInput::new(builder, false),
+        });
+        builder.pop_context();
+
+        // Verifier circuit
+        builder.push_context(log::Level::Info, "verify_message_circuit");
+        info_span!("verify_message_circuit").in_scope(|| {
+            let pk = ECDSAPublicKeyTarget::<Secp256K1>(AffinePointTarget {
+                x: si.pubkey_x.non_native.clone(),
+                y: si.pubkey_y.non_native.clone(),
+            });
+            let msg = si.msg.non_native.clone();
+            let sig = ECDSASignatureTarget::<Secp256K1> {
+                r: si.sig_r.non_native.clone(),
+                s: si.sig_s.non_native.clone(),
+            };
+            verify_message_circuit(builder, msg, sig, pk);
+        });
+        builder.pop_context();
+
+        builder.pop_context();
+        si
+    }
+
+    pub fn set(&self, witness: &mut Inputs, (pk, msg, sig): (PublicKey, MessageHash, Signature)) {
+        self.pubkey_x.set(witness, pk.0.x);
+        self.pubkey_y.set(witness, pk.0.y);
+        self.msg.set(witness, msg);
+        self.sig_r.set(witness, sig.r);
+        self.sig_s.set(witness, sig.s);
+    }
 }
 
 impl Circuit {
     pub fn new(n: usize) -> Circuit {
-        let span = info_span!("Building circuit for for secp256k1 verifications", n);
+        let span = info_span!("Circuit::new", n);
         let _guard = span.enter();
 
         // Configure circuit builder
-        let config = CircuitConfig {
-            ..CircuitConfig::default()
-        };
+        let config = CircuitConfig::standard_ecc_config();
         let mut builder = CircuitBuilder::<F, D>::new(config);
 
         // Start building circuit
-        let span = info_span!("Constructing circuit");
+        let span = info_span!("constructing");
         let constructing_guard = span.enter();
         builder.push_context(log::Level::Info, "Circuit");
 
-        // Inputs
-        builder.push_context(log::Level::Info, "Inputs");
-        let inputs = builder.add_virtual_targets(0);
-        inputs
-            .iter()
-            .for_each(|target| builder.register_public_input(*target));
+        // Verifiers
+        builder.push_context(log::Level::Info, "Verifiers");
+        let inputs = (0..n).map(|_| SignatureInput::new(&mut builder)).collect();
         builder.pop_context();
-
-        // Circuit
-        let outputs = Vec::new();
 
         // Stop building circuit
         drop(constructing_guard);
@@ -59,18 +154,17 @@ impl Circuit {
 
         // Compile circuit
         builder.print_gate_counts(0); // TODO: Add to span
-        let data = info_span!("Compiling circuit").in_scope(|| builder.build::<C>());
+        let data = info_span!("compiling").in_scope(|| builder.build::<C>());
 
-        Self {
-            inputs,
-            outputs,
-            data,
-        }
+        Self { inputs, data }
     }
 
-    pub fn prove(&self, input: impl IntoIterator<Item = GoldilocksField>) -> Result<Proof> {
+    pub fn prove(
+        &self,
+        input: impl IntoIterator<Item = (PublicKey, MessageHash, Signature)>,
+    ) -> Result<Proof> {
         let span = info_span!(
-            "Proving circuit",
+            "proving",
             security_bits = self.data.common.config.security_bits,
             degree = %self.data.common.degree(),
             constraint_degree = %self.data.common.constraint_degree(),
@@ -78,31 +172,37 @@ impl Circuit {
         let _guard = span.enter();
 
         // Set public inputs
-        let pw = span!(Level::INFO, "Set public inputs", n = self.inputs.len()).in_scope(|| {
+        // TODO: Make sure enough inputs are supplied.
+        let pw = span!(Level::INFO, "set_public_inputs", n = self.inputs.len()).in_scope(|| {
             let mut pw = PartialWitness::new();
-            for (&target, value) in self.inputs.iter().zip(input) {
-                pw.set_target(target, value);
+            for (target, value) in self.inputs.iter().zip(input) {
+                target.set(&mut pw, value);
             }
             pw
         });
 
         // Proof
+        let start = Instant::now();
         let proof = {
-            let span = span!(Level::INFO, "Computing compressed proof");
+            let span = span!(Level::INFO, "computing_compressed_proof");
             let _ = span.enter();
             self.data
                 .prove(pw)
                 .and_then(|proof| proof.compress(&self.data.common))
                 .map_err(|e| eyre!(e))?
         };
+        let proof_time = start.elapsed();
         let proof_bytes = proof.to_bytes().map_err(|e| eyre!(e))?;
         span.record("proof_size", &proof_bytes.len());
+        println!("Batch size: {}", self.inputs.len());
+        println!("Proof size: {}", proof_bytes.len());
+        println!("Proof time: {:4}.{:09}", proof_time.as_secs(), proof_time.subsec_nanos());
 
         Ok(proof)
     }
 
     pub fn verify(&self, proof: Proof) -> Result<()> {
-        let span = info_span!("Verifying compressed proof");
+        let span = info_span!("verifying");
         let _guard = span.enter();
 
         // Uncompress proof
--- src/lib.rs
+++ src/lib.rs
@@ -3,22 +3,49 @@
 
 mod circuit;
 
+use std::iter::repeat_with;
+
 use self::circuit::Circuit;
 use clap::Parser;
 use eyre::Result;
+use plonky2::field::types::Field;
+use plonky2_ecdsa::curve::{
+    curve_types::Curve as TCurve,
+    ecdsa::{sign_message, verify_message, ECDSAPublicKey, ECDSASecretKey, ECDSASignature},
+    secp256k1::Secp256K1,
+};
 use tracing::instrument;
 
 #[derive(Clone, Debug, Parser)]
-pub struct Options {}
+pub struct Options {
+    /// The number of signatures in the batch.
+    #[clap(long, default_value = "10")]
+    pub size: usize,
+}
 
-#[instrument]
+type Curve = Secp256K1;
+type PublicKey = ECDSAPublicKey<Curve>;
+type MessageHash = <Curve as TCurve>::ScalarField;
+type Signature = ECDSASignature<Curve>;
+
+fn test_signature() -> (PublicKey, MessageHash, Signature) {
+    type Field = <Curve as TCurve>::ScalarField;
+    let secret_key = ECDSASecretKey(Field::rand());
+    let public_key = secret_key.to_public();
+    let message_hash = MessageHash::rand();
+    let signature = sign_message(message_hash, secret_key);
+    assert!(verify_message(message_hash, signature, public_key));
+    (public_key, message_hash, signature)
+}
+
+#[allow(clippy::missing_errors_doc)]
+#[allow(clippy::unused_async)]
 pub async fn main(options: Options) -> Result<()> {
-    let circuit = Circuit::new(5);
-
-    let proof = circuit.prove([])?;
-
+    let n = options.size;
+    let circuit = Circuit::new(n);
+    let input = repeat_with(test_signature).take(n);
+    let proof = circuit.prove(input)?;
     circuit.verify(proof)?;
-
     Ok(())
 }
 
